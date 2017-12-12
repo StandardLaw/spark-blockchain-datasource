4ecabf6f1f171b75f42a88b5300740d80a76a5f1
Lior Regev <lior@endor.com>
2017-12-12 14:42:19 +0200

    Making helpers not private

--- src/main/scala/com/liorregev/spark/blockchain/blockchain.scala
+++ src/main/scala/com/liorregev/spark/blockchain/blockchain.scala
@@ -3,12 +3,12 @@ package com.liorregev.spark
 import java.nio.{ByteBuffer, ByteOrder}
 
 package object blockchain {
-  private[blockchain] implicit class Unhex(val value: String) extends AnyVal {
+  implicit class Unhex(val value: String) extends AnyVal {
     def bytes: Array[Byte] = value.grouped(2).map(Integer.parseInt(_, 16).toByte).toArray
   }
 
   @SuppressWarnings(Array("org.wartremover.warts.ArrayEquals"))
-  private[blockchain] implicit class ByteArrayOps(val value: Array[Byte]) extends AnyVal {
+  implicit class ByteArrayOps(val value: Array[Byte]) extends AnyVal {
     def hex: String = value.map("%02x" format _).mkString
     def asLong: Long = {
       val padded = Array.fill[Byte](java.lang.Long.BYTES - value.length){0} ++ value
@@ -21,7 +21,7 @@ package object blockchain {
     def asBigInt: BigInt = BigInt(value)
   }
 
-  private[blockchain] implicit class Bytes(val value: Long) extends AnyVal {
+  implicit class Bytes(val value: Long) extends AnyVal {
     def bytes: Array[Byte] = ByteBuffer
       .allocate(java.lang.Long.BYTES)
       .order(ByteOrder.BIG_ENDIAN)
@@ -30,7 +30,7 @@ package object blockchain {
       .dropWhile(_ == 0)
   }
 
-  private[blockchain] implicit class BigIntOps(val value: BigInt) extends AnyVal {
+  implicit class BigIntOps(val value: BigInt) extends AnyVal {
     def exp(exponent: Int): BigInt = value * BigInt(Math.pow(10, exponent.toDouble).toLong)
   }
 }
