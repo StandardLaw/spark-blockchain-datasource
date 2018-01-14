22e4878794446d336000305244586d81ea8fb5d7
Lior Regev <lior@endor.com>
2018-01-14 18:30:08 +0200

    Fixing wart remover errors

--- src/main/scala/com/endor/spark/blockchain/ethereum/token/metadata/CompositeTokenMetadataScraper.scala
+++ src/main/scala/com/endor/spark/blockchain/ethereum/token/metadata/CompositeTokenMetadataScraper.scala
@@ -2,9 +2,11 @@ package com.endor.spark.blockchain.ethereum.token.metadata
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class CompositeTokenMetadataScraper(underlyingScrapers: TokenMetadataScraper*) extends TokenMetadataScraper {
+class CompositeTokenMetadataScraper(underlyingScraper: TokenMetadataScraper, underlyingScrapers: TokenMetadataScraper*)
+  extends TokenMetadataScraper {
+  @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
   override def scrapeAddress(address: String)(implicit ec: ExecutionContext): Future[TokenMetadata] =
-    underlyingScrapers.map(_.scrapeAddress(address)).reduce {
+    (Seq(underlyingScraper) ++ underlyingScrapers).map(_.scrapeAddress(address)).reduce {
       (a, b) => a recoverWith {
         case _ => b
       }
